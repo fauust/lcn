4c3687769b5f14ae8857efe8cdd44162d546ef55
Sébastien Journiac <152491383+Groughy@users.noreply.github.com>
2024-06-05 16:07:07 +0200

    Le script marche !

---
+++ SébastienJourniac/commandevm.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+mkdir -p /home/grough/.ssh
+echo "SHA256:iA8MwsVTDcujzpsfBBIeWLDDWUDqEGyK5zQiKrxaemk journiac@pc-de-sebastien" > /home/grough/.ssh/authorized_keys
+chmod 600 /home/grough/.ssh/authorized_keys
+chown -R grough:grough /home/grough/.ssh
+echo 'grough ALL=(ALL) NOPASSWD: ALL' | sudo tee /etc/sudoers.d/grough
+chmod 440 /etc/sudoers.d/grough
--- SébastienJourniac/install.sh
+++ SébastienJourniac/install.sh
@@ -5,8 +5,9 @@ virt-install --connect=qemu:///system \
  --vcpus=2 \
  --disk size=20,path="/var/lib/libvirt/images/vmtest.qcow2,bus=virtio,cache=none" \
  --initrd-inject=preseed.cfg \
+ --initrd-inject=commandevm.sh \
  --noautoconsole \
- --location=/home/journiac/Téléchargements/debian-12.5.0-amd64-netinst.iso \
+ --location http://ftp.debian.org/debian/dists/bookworm/main/installer-amd64/ \
  --graphics spice \
  --noautoconsole \
  --autostart \
