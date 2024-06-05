ea86f32d0e34f5c1347ba1b163711e446781b199
Sébastien Journiac <152491383+Groughy@users.noreply.github.com>
2024-06-05 14:03:31 +0200

    On répare les problèmes ! On symplifie le code

--- SébastienJourniac/install.sh
+++ SébastienJourniac/install.sh
@@ -1,14 +1,13 @@
-#!/bin/sh
-virt-install \
-  --console=pty,target_type=serial \
-  --disk=pool=default,size=20,format=qcow2,bus=virtio \
-  --extra-args="auto console=ttyS0, 115200n8 serial" \
-  --extra-args="ks=file:preseed.cfg" \
-  --graphics=none \
-  --initrd-inject=preseed.cfg \
-  --location=/home/journiac/Téléchargements/debian-12.5.0-amd64-netinst.iso \
-  --memory=4096 \
-  --name=new-vm \
-  --network=default,model=virtio \
-  --os-variant=debian12 \
-  --vcpus=4
+#!/usr/bin/env bash
+virt-install --connect=qemu:///system \
+ --name="vmtest" \
+ --ram=2048 \
+ --vcpus=2 \
+ --disk size=20,path="/var/lib/libvirt/images/vmtest.qcow2,bus=virtio,cache=none" \
+ --initrd-inject=preseed.cfg \
+ --noautoconsole \
+ --location=/home/journiac/Téléchargements/debian-12.5.0-amd64-netinst.iso \
+ --graphics spice \
+ --noautoconsole \
+ --autostart \
+ --wait
