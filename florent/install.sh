9eacdc4b436b6b3d6873559b95f38916f4f018b8
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-06-05 13:22:17 +0200

    FEAT: correcting the code in install.sh

--- florent/install.sh
+++ florent/install.sh
@@ -1,9 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 virt-install \
 	--console=pty,target_type=serial \
 	--disk=pool=default,size=20,format=qcow2,bus=virtio \
 	--extra-args="auto console=ttyS0, 115200n8 serial" \
-	--extra-args="ks=file:preseed.cfg" \
 	--graphics=none \
 	--initrd-inject=preseed.cfg \
 	--location=/home/florent/Téléchargements/debian-12.5.0-amd64-netinst.iso \
