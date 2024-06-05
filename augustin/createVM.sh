3232dce44745eec21ef0283532987328e7429cd2
AugustinPech <80385451+AugustinPech@users.noreply.github.com>
2024-06-05 09:21:14 +0200

    \# VM creator script with its preseed
    Script works and look nice, some improvments are in order tho :
    
    * the VmName, UserName and PassWord are pre-set
    
    it should be better with some kind of user input.

---
+++ augustin/createVM.sh
@@ -0,0 +1,60 @@
+#! /bin/bash
+#! /usr/bin/env bash
+
+function askName {
+    echo "Enter the name of the VM: "
+    read name
+    echo $name
+}
+function dropExistingVM {
+    virsh destroy $name
+    virsh undefine $name
+    rm -f /var/lib/libvirt/images/debian*.qcow2
+    rm -f /var/lib/libvirt/images/*test*.qcow2
+    rm -f /var/lib/libvirt/images/$name.qcow2
+} 
+function createVM {
+    virt-install \
+     --name=$name \
+     --autoconsole none \
+     --initrd-inject=first_preseed.cfg \
+     --extra-args="auto=true priority=critical preseed/file=/first_preseed.cfg" \
+     --os-variant=debian12 \
+     --disk size=10 --vcpu=2 --ram=2048 --graphics spice \
+     --location=/var/lib/libvirt/images/debian-12.5.0-amd64-netinst.iso \
+     --network bridge:virbr0
+}
+function shutdownVM {
+    virsh shutdown $name
+}
+function startVM {
+    virsh start $name
+}
+
+# may ask for name here
+#name=$(askName)
+name=VMtest
+
+# drop the existing $name
+# if exists
+if [ -f /var/lib/libvirt/images/VMtest.qcow2 ];
+then
+# if is runnig, shutdown it
+    virsh list --all | grep $name
+    if [ $? -eq 0 ];
+    then
+        shutdownVM
+    fi
+
+# uncomment the following line on production
+#    shutdownVM 
+    dropExistingVM
+fi
+
+# create a new $name
+createVM
+
+# start the $name
+startVM
+# comment the following line on production
+# shutdownVM 
