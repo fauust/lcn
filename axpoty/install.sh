3ffff196032d6afcd111bc3bc412ca9e53910470
axpoty <axel.poty@le-campus-numerique.fr>
2024-06-05 12:59:13 +0200

    Automatic debian vm deployment
    This include:
      - VM automated creation (Debian 12)
      - SSH key deployment
      - Sudo NOPASSWD setup

--- axpoty/install.sh
+++ axpoty/install.sh
@@ -6,20 +6,14 @@ virt-install \
 	--ram=2048 \
 	--location="http://ftp.fr.debian.org/debian/dists/Debian12.5/main/installer-amd64/" \
 	--initrd-inject=preseed.cfg \
-	--initrd-inject=laravel.sh \
-	--initrd-inject=vm-app.conf \
+	--initrd-inject=postinst.sh \
+	--initrd-inject=/home/axpoty/.ssh/id_ed25519.pub \
 	--extra-args="auto console=ttyS0,115200n8 serial" \
 	--graphics none \
 	--noautoconsole \
 	--wait -1 \
 	--console pty,target_type=serial \
-	--extra-args="ks=file:/preseed.cfg" \
 	--network default,model=virtio \
 	--os-type=linux \
 	--os-variant=debian12 \
-	--disk=pool=default,size=50,format=qcow2,bus=virtio \
-	--debug
-#scp laravel.sh axpoty@192.168.122.42:/home/axpoty/
-#scp vm-app.conf axpoty@192.168.122.42:/home/axpoty/
-#ssh axpoty@192.168.122.42
-#chmod +x /home/axpoty/laravel.sh
+	--disk=pool=default,size=50,format=qcow2,bus=virtio
