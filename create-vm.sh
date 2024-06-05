ac7d108b4fe3b0fe51ecb6c216e51794a1ed09d5
jonathan-luscap <jonathan.luscap@le-campus-numerique.fr>
2024-06-05 16:31:48 +0200

    just vm third try

--- create-vm.sh
+++ create-vm.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
-
-
 virt-install --name=vm-try01 \
-	--vcpus=2 \
-	--noautoconsole \
-	--memory=2048 \
-	--location /home/jon/Downloads/debian-12.5.0-amd64-netinst.iso \
-	--disk size=10 \
-	--os-variant=debian12 \
-	--wait -1
+  --vcpus=2 \
+  --initrd-inject=preseed.cfg \
+  --noautoconsole \
+  --memory=2048 \
+  --location /home/jon/Downloads/debian-12.5.0-amd64-netinst.iso \
+  --disk size=10 \
+  --os-variant=debian12 \
+  --wait -1
