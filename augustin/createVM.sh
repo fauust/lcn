1721bfb2fc47c3ff3144017b8decb3fe96d75a01
AugustinPech <80385451+AugustinPech@users.noreply.github.com>
2024-06-05 09:40:58 +0200

    testing the pre-commit tool show me what you have got

--- augustin/createVM.sh
+++ augustin/createVM.sh
@@ -1,34 +1,33 @@
-#! /bin/bash
 #! /usr/bin/env bash
 
 function askName {
-    echo "Enter the name of the VM: "
-    read name
-    echo $name
+ echo "Enter the name of the VM: "
+ read -r name
+ echo "$name"
 }
 function dropExistingVM {
-    virsh destroy $name
-    virsh undefine $name
-    rm -f /var/lib/libvirt/images/debian*.qcow2
-    rm -f /var/lib/libvirt/images/*test*.qcow2
-    rm -f /var/lib/libvirt/images/$name.qcow2
+ virsh destroy $name
+ virsh undefine $name
+ rm -f /var/lib/libvirt/images/debian*.qcow2
+ rm -f /var/lib/libvirt/images/*test*.qcow2
+ rm -f /var/lib/libvirt/images/$name.qcow2
 } 
 function createVM {
-    virt-install \
-     --name=$name \
-     --autoconsole none \
-     --initrd-inject=first_preseed.cfg \
-     --extra-args="auto=true priority=critical preseed/file=/first_preseed.cfg" \
-     --os-variant=debian12 \
-     --disk size=10 --vcpu=2 --ram=2048 --graphics spice \
-     --location=/var/lib/libvirt/images/debian-12.5.0-amd64-netinst.iso \
-     --network bridge:virbr0
+ virt-install \
+  --name=$name \
+  --autoconsole none \
+  --initrd-inject=first_preseed.cfg \
+  --extra-args="auto=true priority=critical preseed/file=/first_preseed.cfg" \
+  --os-variant=debian12 \
+  --disk size=10 --vcpu=2 --ram=2048 --graphics spice \
+  --location=/var/lib/libvirt/images/debian-12.5.0-amd64-netinst.iso \
+  --network bridge:virbr0
 }
 function shutdownVM {
-    virsh shutdown $name
+ virsh shutdown $name
 }
 function startVM {
-    virsh start $name
+ virsh start $name
 }
 
 # may ask for name here
@@ -40,15 +39,14 @@ name=VMtest
 if [ -f /var/lib/libvirt/images/VMtest.qcow2 ];
 then
 # if is runnig, shutdown it
-    virsh list --all | grep $name
-    if [ $? -eq 0 ];
-    then
-        shutdownVM
-    fi
+ if ! virsh list --all | grep $name
+ then
+  shutdownVM
+ fi
 
 # uncomment the following line on production
-#    shutdownVM 
-    dropExistingVM
+# shutdownVM 
+ dropExistingVM
 fi
 
 # create a new $name
