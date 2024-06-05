6e1c103ecc18ec402fda0886f8d056ceaf4487a4
AugustinPech <80385451+AugustinPech@users.noreply.github.com>
2024-06-05 11:36:22 +0200

    VM creator script & preseed
    I choosed to make a creator script with functions to increase readability.
    
    The preseed file is a bit raw, I don't understand it all yet.
    
    For now it creates a VM with the openssh package.
    
    I am working on another script to copy the SSH key on the VM.
    
    I will merge the tow scripts later.
    
    Script works and look nice, some improvments are in order tho :
    
    * the VmName, UserName and PassWord are pre-set
    
    It should be better with some kind of user input : file or form

--- augustin/createVM.sh
+++ augustin/createVM.sh
@@ -1,58 +1,56 @@
 #! /usr/bin/env bash
 
 function askName {
- echo "Enter the name of the VM: "
- read -r name
- echo "$name"
+	echo "Enter the name of the VM: "
+	read -r name
+	echo "$name"
 }
 function dropExistingVM {
- virsh destroy $name
- virsh undefine $name
- rm -f /var/lib/libvirt/images/debian*.qcow2
- rm -f /var/lib/libvirt/images/*test*.qcow2
- rm -f /var/lib/libvirt/images/$name.qcow2
-} 
+	virsh destroy "$name"
+	virsh undefine "$name"
+	rm -f /var/lib/libvirt/images/debian*.qcow2
+	rm -f /var/lib/libvirt/images/*test*.qcow2
+	rm -f /var/lib/libvirt/images/"$name".qcow2
+}
 function createVM {
- virt-install \
-  --name=$name \
-  --autoconsole none \
-  --initrd-inject=first_preseed.cfg \
-  --extra-args="auto=true priority=critical preseed/file=/first_preseed.cfg" \
-  --os-variant=debian12 \
-  --disk size=10 --vcpu=2 --ram=2048 --graphics spice \
-  --location=/var/lib/libvirt/images/debian-12.5.0-amd64-netinst.iso \
-  --network bridge:virbr0
+	virt-install \
+		--name="$name" \
+		--autoconsole none \
+		--initrd-inject=first_preseed.cfg \
+		--extra-args="auto=true priority=critical preseed/file=/first_preseed.cfg" \
+		--os-variant=debian12 \
+		--disk size=10 --vcpu=2 --ram=2048 --graphics spice \
+		--location=/var/lib/libvirt/images/debian-12.5.0-amd64-netinst.iso \
+		--wait -1 \
+		--network bridge:virbr0
 }
 function shutdownVM {
- virsh shutdown $name
+	virsh shutdown "$name"
 }
 function startVM {
- virsh start $name
+	virsh start "$name"
 }
 
 # may ask for name here
 #name=$(askName)
 name=VMtest
 
-# drop the existing $name
+# drop the existing "$name"
 # if exists
-if [ -f /var/lib/libvirt/images/VMtest.qcow2 ];
-then
-# if is runnig, shutdown it
- if ! virsh list --all | grep $name
- then
-  shutdownVM
- fi
-
-# uncomment the following line on production
-# shutdownVM 
- dropExistingVM
+if [ -f /var/lib/libvirt/images/VMtest.qcow2 ]; then
+	# if is running, shutdown it
+	if ! virsh list --all | grep "$name"; then
+		shutdownVM
+	fi
+	# uncomment the following line on production
+	# shutdownVM
+	dropExistingVM
 fi
 
-# create a new $name
+# create a new "$name"
 createVM
 
-# start the $name
+# start the "$name"
 startVM
 # comment the following line on production
-# shutdownVM 
+# shutdownVM
