00ac6b49ecac4c7f7c2e475f3ee17059186ddf65
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-06-06 15:00:40 +0200

    create ssh.sh and sudoers.sh refacto install.sh and preseed.cfg

--- florent/install.sh
+++ florent/install.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/env bash
-virt-install \
-	--connect=qemu:///system \
-	--console=pty,target_type=serial \
-	--disk=pool=default,size=20,format=qcow2,bus=virtio \
-	--extra-args="auto console=ttyS0, 115200n8 serial" \
-	--graphics=none \
-	--initrd-inject=preseed.cfg \
-	--initrd-inject=postinst.sh \
-	--location=http://ftp.debian.org/debian/dists/bookworm/main/installer-amd64/ \
-	--memory=4096 \
-	--name=vm-deb \
-	--network=default,model=virtio \
-	--os-variant=debian12 \
-	--vcpus=4 \
-	--noautoconsole \
-	--wait
+
+# ssh-keygen -f "/home/florent/.ssh/known_hosts" -R "192.168.122.66"
+
+# virt-install \
+# 	--connect=qemu:///system \
+# 	--console=pty,target_type=serial \
+# 	--disk=pool=default,size=20,format=qcow2,bus=virtio \
+# 	--extra-args="auto console=ttyS0, 115200n8 serial" \
+# 	--graphics=none \
+# 	--initrd-inject=preseed.cfg \
+# 	--initrd-inject=ssh.sh \
+# 	--initrd-inject=sudoers.sh \
+# 	--location=http://ftp.debian.org/debian/dists/bookworm/main/installer-amd64/ \
+# 	--memory=4096 \
+# 	--name=vm-deb \
+# 	--network=default,model=virtio \
+# 	--os-variant=debian12 \
+# 	--vcpus=4 \
+# 	--noautoconsole \
+# 	--wait
+
+sudo virsh snapshot-revert vm-deb snapshot0
+scp -o "StrictHostKeyChecking no" php.sh florent@192.168.122.66:
+ssh -o "StrictHostKeyChecking no" florent@192.168.122.66 "sudo bash ./php.sh"
