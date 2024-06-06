32d3a30258693e252e0b66c762f607a244372246
Sébastien Journiac <152491383+Groughy@users.noreply.github.com>
2024-06-06 14:27:14 +0200

    Création snapshot Installation mariadb

--- SébastienJourniac/install.sh
+++ SébastienJourniac/install.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
-ssh-keygen -f "/home/journiac/.ssh/known_hosts" -R "192.168.122.51"
 
-virt-install --connect=qemu:///system \
- --name="vmtest" \
- --ram=2048 \
- --vcpus=2 \
- --disk size=20,path="/var/lib/libvirt/images/vmtest.qcow2,bus=virtio,cache=none" \
- --initrd-inject=preseed.cfg \
- --initrd-inject=commandevm.sh \
- --initrd-inject=installapache.sh \
- --initrd-inject=installphp.sh \
- --noautoconsole \
- --location http://ftp.debian.org/debian/dists/bookworm/main/installer-amd64/ \
- --graphics spice \
- --noautoconsole \
- --autostart \
- --wait
+# ssh-keygen -f "/home/journiac/.ssh/known_hosts" -R "192.168.122.51"
+
+# virt-install --connect=qemu:///system \
+#  --name="vmtest" \
+#  --ram=2048 \
+#  --vcpus=2 \
+#  --disk size=20,path="/var/lib/libvirt/images/vmtest.qcow2,bus=virtio,cache=none" \
+#  --initrd-inject=preseed.cfg \
+#  --initrd-inject=commandevm.sh \
+#  --noautoconsole \
+#  --location http://ftp.debian.org/debian/dists/bookworm/main/installer-amd64/ \
+#  --graphics spice \
+#  --noautoconsole \
+#  --autostart \
+#  --wait
+
+sudo virsh snapshot-revert vmtest snapshot1
+scp -o "StrictHostKeyChecking no" install_mariadb.sh grough@192.168.122.51:
+ssh -o "StrictHostKeyChecking no" grough@192.168.122.51 "sudo bash ./install_mariadb.sh"
