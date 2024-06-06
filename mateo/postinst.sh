9c281e36aced6e29742d4594186686034b299822
MateoNicoud <mateo.nicoud@le-campus-numerique.fr>
2024-06-06 12:23:09 +0200

    test

---
+++ mateo/postinst.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Ajouter l'utilisateur "utilisateur1" aux sudoers
+apt update
+mkdir -p /home/mateo-nicoud/.ssh
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDlQCXgHItq5iEa8bZEWflEWJkQl7aAZyQzAEbABDfvOzB4HBqaUt+gx+XR6DmpXEXYd6kdcfVH1TAbWRQ7Enl2vxYHELBbfpV2/l4+8HYIyEHaO6q2uCwrNAhfTw2W6bUttTqM6h1QH49tHstpuwbzaP/oa9pS0AqhTBi9st7uEfApA+hg/ymGnkVHENWLysXkNJcfR41oyOvxx2xjiY5UvtxORbMr6lYkz+zB2/DuDYzwFrUIPDCmRwu2JsOxfzPIWHBmmrjJZoaQzEzIJP7oJB/3XUSp9F8XYlE7GD9oaYAglcU7j7lZq6Qv1p81bdbu7bsF7Ctho1QevjurpQS7CsDnL16/afOl6s+j6wtYrkdU/NTbCp2Z5V5pJuBdkG4abNASENTZ+/hUkr6dBviOzhTwB41xoCCPQ+6nA4+RO7zSYmKk98rliJp/zBCmaHzTtjPgKOzTOQ3V4TOLbkjCS1RH/2ysSqqTzyUE9CkGa4Slfb3W4l0qoSs6nDi+40M= mateo-nicoud@mateonicoud-desktop">> /home/mateo-nicoud/.ssh/authorized_keys
+chown mateo-nicoud:mateo-nicoud /home/mateo-nicoud/.ssh
+chown mateo-nicoud:mateo-nicoud /home/mateo-nicoud/.ssh/authorized_keys
+chmod 700 /home/mateo-nicoud/.ssh
+chmod 600 /home/mateo-nicoud/.ssh/authorized_keys
+apt install -y openssh-server
+apt install -y sudo
+apt install -y vim
+apt install -y composer
+apt install -y php-fpm
+apt install -y apache2
+apt install -y mariadb
+
+
+
+sudo a2enmod proxy_fcgi setenvif
+sudo chmod -R 775 /etc/apache2/sites-available/
+sudo chown -R root:www-data /etc/apache2/sites-available
+
+echo "mateo-nicoud ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
+
