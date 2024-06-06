6b9949ec4f3e22627e984537deb514fae2d44a6d
Sébastien Journiac <152491383+Groughy@users.noreply.github.com>
2024-06-06 11:57:08 +0200

    On nettoie la PR

--- SébastienJourniac/install.sh
+++ SébastienJourniac/install.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+ssh-keygen -f "/home/journiac/.ssh/known_hosts" -R "192.168.122.51"
+
 virt-install --connect=qemu:///system \
  --name="vmtest" \
  --ram=2048 \
@@ -6,6 +8,8 @@ virt-install --connect=qemu:///system \
  --disk size=20,path="/var/lib/libvirt/images/vmtest.qcow2,bus=virtio,cache=none" \
  --initrd-inject=preseed.cfg \
  --initrd-inject=commandevm.sh \
+ --initrd-inject=installapache.sh \
+ --initrd-inject=installphp.sh \
  --noautoconsole \
  --location http://ftp.debian.org/debian/dists/bookworm/main/installer-amd64/ \
  --graphics spice \
