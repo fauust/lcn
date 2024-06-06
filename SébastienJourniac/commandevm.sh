9dd75c063f453cee1b7ef82a967186e03caadc55
Sébastien Journiac <152491383+Groughy@users.noreply.github.com>
2024-06-06 11:06:15 +0200

    SSh sans mot de passe

--- SébastienJourniac/commandevm.sh
+++ SébastienJourniac/commandevm.sh
@@ -2,6 +2,7 @@
 mkdir -p /home/grough/.ssh
 echo "SHA256:iA8MwsVTDcujzpsfBBIeWLDDWUDqEGyK5zQiKrxaemk journiac@pc-de-sebastien" > /home/grough/.ssh/authorized_keys
 chmod 600 /home/grough/.ssh/authorized_keys
+chmod 700 /home/grough/.ssh
 chown -R grough:grough /home/grough/.ssh
 echo 'grough ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/grough
 chmod 440 /etc/sudoers.d/grough
