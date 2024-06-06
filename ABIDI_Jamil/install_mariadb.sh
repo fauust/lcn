93177922be18893ce8ae26e01b754019ed9f40a2
jamil abidi <jamil.abidi@le-campus-numerique.fr>
2024-06-06 15:13:25 +0200

    getting rid of this secure install horsehit (thanks to the man)

--- ABIDI_Jamil/install_mariadb.sh
+++ ABIDI_Jamil/install_mariadb.sh
@@ -2,27 +2,15 @@
 # shellcheck disable=SC1091
 . ./function_post_install.sh
 # shellcheck disable=SC2209
-DEFAULTPASSWD=test
-MARIADB_ROOT_PWD="${1:-$DEFAULTPASSWD}"
+
 
 
 
 # Install and configure Maria-DB
 print_color "green" "Installing MariaDB Server.."
 apt install -y mariadb-server
-#OK that one sucks but we're on the clock
-#TODO: making a less horrible syntax
-sudo mariadb-secure-installation <<-EOF
-$MARIADB_ROOT_PWD
-n
-n
-Y
-Y
-Y
-Y
-EOF
 
 print_color "green" "Starting MariaDB Server.."
-sudo service mariadb start
-sudo systemctl enable mariadb
+service mariadb start
+systemctl enable mariadb
 check_service_status mariadb
