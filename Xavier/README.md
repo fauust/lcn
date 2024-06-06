21f1ccdeae85b4e7e659840f9717a0554cc0f3e0
xavier <xavier.figari@le-campus-numerique.fr>
2024-06-06 13:42:17 +0200

    Added double quotes in shell script, clean up Markdown

--- Xavier/README.md
+++ Xavier/README.md
@@ -1,8 +1,12 @@
 # Create a Debian VM with KVM
-Demonstrate the unattended creation of a Debian virtual machine, using the following tools :
+
+Demonstrate the unattended creation of a Debian virtual machine,
+using the following tools :
+
 - virt-install
 - Debian preseed configuration file
 
 For now, the script creates a virtual machine with:
+
 - A Debian stable netinst ISO (Bookworm, Debian12)
 - one user, no root
--- Xavier/postinst.sh
+++ Xavier/postinst.sh
@@ -6,12 +6,12 @@ user="$1"
 key="$2"
 
 # Copy public key
-mkdir -p /home/$user/.ssh
-echo $key > /home/$user/.ssh/authorized_keys
+mkdir -p /home/"$user"/.ssh
+echo "$key" > /home/"$user"/.ssh/authorized_keys
 # Set rights
-chown -R $user:$user /home/$user/.ssh
-chmod 600 /home/$user/.ssh/authorized_keys
+chown -R "$user":"$user" /home/"$user"/.ssh
+chmod 600 /home/"$user"/.ssh/authorized_keys
 
 # Allow user to enter sudo commands with no password
-echo "$user ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/$user
-chmod 440 /etc/sudoers.d/$user
+echo "$user ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/"$user"
+chmod 440 /etc/sudoers.d/"$user"
