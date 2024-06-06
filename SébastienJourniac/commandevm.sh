0ead67a9af69ab26ebe2e43b61556597775ef9f2
Sébastien Journiac <152491383+Groughy@users.noreply.github.com>
2024-06-06 09:03:43 +0200

    Retouche preseed et script

--- SébastienJourniac/commandevm.sh
+++ SébastienJourniac/commandevm.sh
@@ -3,5 +3,5 @@ mkdir -p /home/grough/.ssh
 echo "SHA256:iA8MwsVTDcujzpsfBBIeWLDDWUDqEGyK5zQiKrxaemk journiac@pc-de-sebastien" > /home/grough/.ssh/authorized_keys
 chmod 600 /home/grough/.ssh/authorized_keys
 chown -R grough:grough /home/grough/.ssh
-echo 'grough ALL=(ALL) NOPASSWD: ALL' | sudo tee /etc/sudoers.d/grough
+echo 'grough ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/grough
 chmod 440 /etc/sudoers.d/grough
