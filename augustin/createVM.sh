abe31af147b17e823b2574823a30e9f457941369
AugustinPech <80385451+AugustinPech@users.noreply.github.com>
2024-06-06 10:47:15 +0200

    create a VM with ssh and sudo no pass word i created a file with password, VMname and userName which i read and split in a way i am pretty proud of.

--- augustin/createVM.sh
+++ augustin/createVM.sh
@@ -1,56 +1,51 @@
 #! /usr/bin/env bash
+name=$1
+userName=$2
 
-function askName {
-	echo "Enter the name of the VM: "
-	read -r name
-	echo "$name"
+function shutdownVM {
+  ! virsh list --all | grep "$name" && virsh shutdown "$name"
 }
 function dropExistingVM {
-	virsh destroy "$name"
-	virsh undefine "$name"
-	rm -f /var/lib/libvirt/images/debian*.qcow2
-	rm -f /var/lib/libvirt/images/*test*.qcow2
-	rm -f /var/lib/libvirt/images/"$name".qcow2
+  virsh destroy "$name"
+  virsh undefine "$name"
+  # dangerous but useful if the *.qcow2 are stacking up in the memory
+  # sudo rm -f /var/lib/libvirt/images/debian*.qcow2
+  # sudo rm -f /var/lib/libvirt/images/*test*.qcow2
+  [[ -f /var/lib/libvirt/images/"$name".qcow2 ]] && sudo rm -f /var/lib/libvirt/images/"$name".qcow2 # tofix : need to find a way to remove the sudo
 }
 function createVM {
-	virt-install \
-		--name="$name" \
-		--autoconsole none \
-		--initrd-inject=first_preseed.cfg \
-		--extra-args="auto=true priority=critical preseed/file=/first_preseed.cfg" \
-		--os-variant=debian12 \
-		--disk size=10 --vcpu=2 --ram=2048 --graphics spice \
-		--location=/var/lib/libvirt/images/debian-12.5.0-amd64-netinst.iso \
-		--wait -1 \
-		--network bridge:virbr0
-}
-function shutdownVM {
-	virsh shutdown "$name"
+  if [[ -f /var/lib/libvirt/images/debian-12.5.0-amd64-netinst.iso ]]; then
+    virt-install \
+      --name="$name" \
+      --autoconsole none \
+      --initrd-inject=/home/augustin/Desktop/infra_VM/scripts/"$userName"_preseed.cfg \
+      --initrd-inject=/home/augustin/Desktop/infra_VM/scripts/postinst.sh \
+      --extra-args="auto=true priority=critical preseed/file=/""$userName""_preseed.cfg" \
+      --os-variant=debian12 \
+      --disk size=10 --vcpu=4 --ram=2048 --graphics spice \
+      --location=/var/lib/libvirt/images/debian-12.5.0-amd64-netinst.iso \
+      --wait -1 \
+      --network bridge:virbr0
+  else
+    printf "\nERROR : could not find the file 'debian-12.5.0-amd64-netinst.iso'\nPlease check the directory '/var/lib/libvirt/images/'\nYou may always download debian iso on 'https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-12.5.0-amd64-netinst.iso'"
+  fi
 }
 function startVM {
-	virsh start "$name"
+  virsh start "$name"
 }
 
-# may ask for name here
-#name=$(askName)
-name=VMtest
+# if is running, shutdown
+shutdownVM
 
-# drop the existing "$name"
-# if exists
-if [ -f /var/lib/libvirt/images/VMtest.qcow2 ]; then
-	# if is running, shutdown it
-	if ! virsh list --all | grep "$name"; then
-		shutdownVM
-	fi
-	# uncomment the following line on production
-	# shutdownVM
-	dropExistingVM
-fi
+# if exists, drop
+dropExistingVM
 
 # create a new "$name"
-createVM
-
+if [ -f /home/augustin/Desktop/infra_VM/scripts/"$userName"_preseed.cfg ]; then
+  createVM
+else
+  echo "error"
+fi
 # start the "$name"
 startVM
 # comment the following line on production
-# shutdownVM
