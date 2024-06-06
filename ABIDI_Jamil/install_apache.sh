c65b6711ff1c56942aecef4f79d6023a89c50134
jamil abidi <jamil.abidi@le-campus-numerique.fr>
2024-06-06 13:58:55 +0200

    script installing apache OK

---
+++ ABIDI_Jamil/install_apache.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+. ./function_post_install.sh
+
+#install useful stuff
+apt install -y vim git curl
+#install apache
+# Install web server packages
+print_color "green" "Installing Web Server Packages .."
+apt install -y apache2
+systemctl start apache2
+systemctl enable apache2
+
+#check apache is working ?
+check_service_status apache2
+# check siteweb working
+web_page=$(curl http://localhost)
+item="apache"
+check_item "$web_page" "$item"
