bca0eb3256b530a0960f4a2f595f5a9a03352ce8
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-06-06 09:32:59 +0200

    change the line allowing execute sudo without password

--- florent/postinst.sh
+++ florent/postinst.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-# https://askubuntu.com/questions/147241/execute-sudo-without-password
-echo "florent ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/florent
-
 mkdir -p /home/florent/.ssh
 echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIATZkKU4NbhuJZMZ9WjVkhGms5qtHNNs9hZNXprPKtFI florent@florent-desktop" > /home/florent/.ssh/authorized_keys
 chmod 600 /home/florent/.ssh/authorized_keys
 chown -R florent:florent /home/florent/.ssh
-echo 'florent ALL=(ALL) NOPASSWD: ALL' | sudo tee /etc/sudoers.d/florent
+
+# https://askubuntu.com/questions/147241/execute-sudo-without-password
+echo 'florent ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/florent
+
 chmod 440 /etc/sudoers.d/florent
