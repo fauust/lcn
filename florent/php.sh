42b9bb99fb6f0724f1a480652d556ca7cb29a945
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-06-06 15:33:10 +0200

    modify code of php.sh

--- florent/php.sh
+++ florent/php.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt install -y php-fpm php-common php-curl php-gd php-cli php-mysql php-mbstring php-dom php-xdebug
-sudo systemctl status php8.2-fpm
+apt update -y
+apt upgrade -y
+apt install -y php-fpm php-common php-curl php-gd php-cli php-mysql php-mbstring php-dom php-xdebug
