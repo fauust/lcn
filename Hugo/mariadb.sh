a0abfa0d2775a59bb87682ae936da5765a496af7
HeimyJohn <hugo.chaloyard@le-campus-numerique.fr>
2024-06-07 10:01:21 +0200

    fix: remove the create password

--- Hugo/mariadb.sh
+++ Hugo/mariadb.sh
@@ -9,7 +9,7 @@ PROJECT_NAME=$4
 ### Install MariaDB ###
 sudo apt-get install -y mariadb-server
 sudo mariadb-secure-installation <<EOF
-$DB_ROOT_PASS
+n
 n
 n
 Y
@@ -19,7 +19,7 @@ Y
 EOF
 
 ### Create Database and User ###
-sudo mariadb -u root -p"$DB_ROOT_PASS" <<EOF
+<<EOF
 CREATE DATABASE $PROJECT_NAME;
 CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASS';
 GRANT ALL PRIVILEGES ON $PROJECT_NAME.* TO '$DB_USER'@'localhost';
