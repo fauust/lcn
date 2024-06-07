74b1e7c3ccf2f923e14e33f128709aa69b317cf7
xavier <xavier.figari@le-campus-numerique.fr>
2024-06-07 12:13:56 +0200

    Mariadb installed, sample database created

--- Xavier/install.sh
+++ Xavier/install.sh
@@ -7,7 +7,7 @@ incremental=true # If true, the script will not launch create_vm.sh, but use an 
 # General installation script
 # ===========================
 
-vm_IP="192.168.122.51"
+vm_IP=192.168.122.51
 vm_user="xav"
 vm_host=$vm_user@$vm_IP
 
@@ -30,7 +30,7 @@ else
 fi
 
 # Wait for the VM to reboot (otherwise following scp and ssh commands will fail)
-while nc -nz $vm_IP 22; do
+while ! nc -nz $vm_IP 22; do
     sleep 1
 done
 
@@ -39,11 +39,11 @@ done
 echo "---------------------------------------------"
 echo "Installing software"
 echo "---------------------------------------------"
-scripts=("install_apache.sh" "install_php.sh")
+scripts=("install_apache.sh" "install_php.sh" "install_mariadb.sh")
 for script in "${scripts[@]}"; do
     echo "$script ..."
     eval scp "$ssh_opts" "$script" $vm_host:  &> /dev/null
-    eval ssh "$ssh_opts" $vm_host "sudo ./$script"  &> /dev/null
+    eval ssh "$ssh_opts" $vm_host "sudo ./$script"  # &> /dev/null
 done
 
 # Note : I need to put "eval" before scp and ssh otherwise $ssh_opts is not expanded correctly (it keeps the quotes)
---
+++ Xavier/install_mariadb.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Install Mariadb
+# ========================
+apt-get install -y mariadb-server mariadb-client
+
+# Create a database and a user
+
+user="xav"
+passwd="1234"
+database="db_xav"
+
+mariadb -u root <<-EOF
+CREATE DATABASE $database;
+CREATE user '$user'@'localhost' IDENTIFIED BY '$passwd';
+GRANT ALL ON $database.* TO '$user'@'localhost';
+FLUSH PRIVILEGES;
+USE $database;
+CREATE TABLE contacts (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  age INT(3) NOT NULL,
+  PRIMARY KEY  (id)
+);
+EOF
+
