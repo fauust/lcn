02bbb8e4f0249c430a29f1b634728de20262dd8e
MateoNicoud <mateo.nicoud@le-campus-numerique.fr>
2024-06-07 12:09:04 +0200

    fixed configureApache.sh

--- mateo/configureApache.sh
+++ mateo/configureApache.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Initialisation
-sudo chmod -R 775 /etc/apache2/sites-available/ \
-sudo chown -R root:www-data /etc/apache2/sites-available \
-sudo usermod -aG www-data mateo-nicoud \
-[ -d /var/www/monprojet ] || sudo mkdir /var/www/monprojet \
-sudo chown -R mateo-nicoud:www-data /var/www/monprojet \
-sudo chmod -R 775 /var/www/monprojet \
-[ -d /var/www/monprojet/public ] || mkdir -p /var/www/monprojet/public \
-sudo chmod -R 775 /var/www/monprojet/public \
-sudo chown -R mateo-nicoud:www-data /var/www/monprojet/public \
+sudo chmod -R 775 /etc/apache2/sites-available/
+sudo chown -R root:www-data /etc/apache2/sites-available
+sudo usermod -aG www-data mateo-nicoud
+[ -d /var/www/monprojet ] || sudo mkdir /var/www/monprojet
+sudo chown -R mateo-nicoud:www-data /var/www/monprojet
+sudo chmod -R 775 /var/www/monprojet
+[ -d /var/www/monprojet/public ] || mkdir /var/www/monprojet/public
+sudo chmod -R 775 /var/www/monprojet/public
+sudo chown -R mateo-nicoud:www-data /var/www/monprojet/public
 sudo echo '127.0.0.1 monprojet.local' | sudo tee -a /etc/hosts
