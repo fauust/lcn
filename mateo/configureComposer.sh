ee5e94993c118fb785271bad9715890be0429e06
MateoNicoud <mateo.nicoud@le-campus-numerique.fr>
2024-06-07 13:36:48 +0200

    Deleted useless files

--- mateo/configureComposer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo apt update
-sudo apt -y install composer
-composer create-project --prefer-dist laravel/laravel /var/www/monprojet
-composer install -d /var/www/monprojet
-
-sudo systemctl reload apache2.service
