0291d20bfd9191298f18e094cb45e297a2da00d9
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-06-07 12:08:00 +0200

    FEAT: add loop for launch install script of php, apache... after VM starting

--- florent/install.sh
+++ florent/install.sh
@@ -4,27 +4,27 @@ USER="florent"
 IP="192.168.122.66"
 NC="nc -vz $IP 22"
 
-# ssh-keygen -f "/home/$USER/.ssh/known_hosts" -R $IP
+ssh-keygen -f "/home/$USER/.ssh/known_hosts" -R $IP
 
-# virt-install \
-# 	--connect=qemu:///system \
-# 	--console=pty,target_type=serial \
-# 	--disk=pool=default,size=20,format=qcow2,bus=virtio \
-# 	--extra-args="auto console=ttyS0, 115200n8 serial" \
-# 	--graphics=none \
-# 	--initrd-inject=preseed.cfg \
-# 	--initrd-inject=ssh.sh \
-# 	--initrd-inject=sudoers.sh \
-# 	--location=http://ftp.debian.org/debian/dists/bookworm/main/installer-amd64/ \
-# 	--memory=4096 \
-# 	--name=vm-deb \
-# 	--network=default,model=virtio \
-# 	--os-variant=debian12 \
-# 	--vcpus=4 \
-# 	--noautoconsole \
-# 	--wait
+virt-install \
+	--connect=qemu:///system \
+	--console=pty,target_type=serial \
+	--disk=pool=default,size=20,format=qcow2,bus=virtio \
+	--extra-args="auto console=ttyS0, 115200n8 serial" \
+	--graphics=none \
+	--initrd-inject=preseed.cfg \
+	--initrd-inject=ssh.sh \
+	--initrd-inject=sudoers.sh \
+	--location=http://ftp.debian.org/debian/dists/bookworm/main/installer-amd64/ \
+	--memory=4096 \
+	--name=vm-deb \
+	--network=default,model=virtio \
+	--os-variant=debian12 \
+	--vcpus=4 \
+	--noautoconsole \
+	--wait
 
-while [ "$NC" != 0 ]; do
+while ! $NC; do
     echo "VM starting..."
     sleep 1
 done
