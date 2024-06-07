22b2029b437727a91567fcebb464630832e5aeb0
jamil abidi <jamil.abidi@le-campus-numerique.fr>
2024-06-07 16:05:20 +0200

    Laravel with maraidb Stil nasty work but at least work with mariadb

--- ABIDI_Jamil/install_laravel.sh
+++ ABIDI_Jamil/install_laravel.sh
@@ -26,21 +26,93 @@ yes | composer create-project laravel/laravel example-app
 #initiate project
 cd ./example-app/
 composer install
+cat > .env<<-EOF
+
+APP_NAME=Laravel
+APP_ENV=local
+APP_KEY=base64:ro0rKTJJJWfp5cwy0ZRI6Zd7aqmv/f0NVxH5cUF3tkY=
+APP_DEBUG=true
+APP_TIMEZONE=UTC
+APP_URL=http://localhost
+
+APP_LOCALE=en
+APP_FALLBACK_LOCALE=en
+APP_FAKER_LOCALE=en_US
+
+APP_MAINTENANCE_DRIVER=file
+APP_MAINTENANCE_STORE=database
+
+BCRYPT_ROUNDS=12
+
+LOG_CHANNEL=stack
+LOG_STACK=single
+LOG_DEPRECATIONS_CHANNEL=null
+LOG_LEVEL=debug
+
+DB_CONNECTION=mariadb
+DB_HOST=127.0.0.1
+DB_PORT=3306
+DB_DATABASE=laravel
+DB_USERNAME=test
+DB_PASSWORD=test
+
+SESSION_DRIVER=database
+SESSION_LIFETIME=120
+SESSION_ENCRYPT=false
+SESSION_PATH=/
+SESSION_DOMAIN=null
+
+BROADCAST_CONNECTION=log
+FILESYSTEM_DISK=local
+QUEUE_CONNECTION=database
+
+CACHE_STORE=database
+CACHE_PREFIX=
+
+MEMCACHED_HOST=127.0.0.1
+
+REDIS_CLIENT=phpredis
+REDIS_HOST=127.0.0.1
+REDIS_PASSWORD=null
+REDIS_PORT=6379
+
+MAIL_MAILER=log
+MAIL_HOST=127.0.0.1
+MAIL_PORT=2525
+MAIL_USERNAME=null
+MAIL_PASSWORD=null
+MAIL_ENCRYPTION=null
+MAIL_FROM_ADDRESS="hello@example.com"
+MAIL_FROM_NAME="${APP_NAME}"
+
+AWS_ACCESS_KEY_ID=
+AWS_SECRET_ACCESS_KEY=
+AWS_DEFAULT_REGION=us-east-1
+AWS_BUCKET=
+AWS_USE_PATH_STYLE_ENDPOINT=false
+
+VITE_APP_NAME="${APP_NAME}"
+EOF
+
+#create the table in mariadb:
+cat > myappdb.sql<<-EOF
+CREATE DATABASE laravel;
+CREATE USER 'test'@'localhost' IDENTIFIED BY 'test';
+GRANT ALL PRIVILEGES ON *.* TO 'test'@'localhost';
+FLUSH PRIVILEGES;
+EOF
+
+sudo mariadb  < myappdb.sql
 php artisan migrate --force
 # shellcheck disable=SC2103
 cd ..
 
-
-
-
-#isetting the directory with right rights dans /var/www
+#setting the directory with right rights dans /var/www
 sudo mv ./example-app /var/www/
 sudo chown -R www-data:www-data /var/www/example-app
 
 
 ## configure apache
-
-
 
 cat > my-app.conf<<-EOF
 <VirtualHost *:80>
