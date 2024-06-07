6be1b49c2a9002b41d898cca3e64b0d5ba6e158a
MateoNicoud <mateo.nicoud@le-campus-numerique.fr>
2024-06-07 14:23:18 +0200

    Syntaxe Correction in postinst.sh

--- mateo/postinst.sh
+++ mateo/postinst.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
 # Ajouter l'utilisateur "utilisateur1" aux sudoers
-apt update
 mkdir -p /home/mateo-nicoud/.ssh
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDlQCXgHItq5iEa8bZEWflEWJkQl7aAZyQzAEbABDfvOzB4HBqaUt+gx+XR6DmpXEXYd6kdcfVH1TAbWRQ7Enl2vxYHELBbfpV2/l4+8HYIyEHaO6q2uCwrNAhfTw2W6bUttTqM6h1QH49tHstpuwbzaP/oa9pS0AqhTBi9st7uEfApA+hg/ymGnkVHENWLysXkNJcfR41oyOvxx2xjiY5UvtxORbMr6lYkz+zB2/DuDYzwFrUIPDCmRwu2JsOxfzPIWHBmmrjJZoaQzEzIJP7oJB/3XUSp9F8XYlE7GD9oaYAglcU7j7lZq6Qv1p81bdbu7bsF7Ctho1QevjurpQS7CsDnL16/afOl6s+j6wtYrkdU/NTbCp2Z5V5pJuBdkG4abNASENTZ+/hUkr6dBviOzhTwB41xoCCPQ+6nA4+RO7zSYmKk98rliJp/zBCmaHzTtjPgKOzTOQ3V4TOLbkjCS1RH/2ysSqqTzyUE9CkGa4Slfb3W4l0qoSs6nDi+40M= mateo-nicoud@mateonicoud-desktop">> /home/mateo-nicoud/.ssh/authorized_keys
 chown mateo-nicoud:mateo-nicoud /home/mateo-nicoud/.ssh
 chown mateo-nicoud:mateo-nicoud /home/mateo-nicoud/.ssh/authorized_keys
 chmod 700 /home/mateo-nicoud/.ssh
 chmod 600 /home/mateo-nicoud/.ssh/authorized_keys
+
+# Mettre à jour la liste des paquets
+apt update
+
+# Installer les paquets nécessaires
 apt install -y \
   openssh-server \
   sudo \
   vim \
   php8.2-fpm \
   php-xml \
-  php-dom \
+  php-xmlrpc \
   apache2 \
-  mariadb \
+  mariadb-server \
   php-mysql
 
 a2dismod mpm_prefork
