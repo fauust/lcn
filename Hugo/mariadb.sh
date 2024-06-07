fae648f4ecdfe9d52e372b77f5d2facd1ed72d66
HeimyJohn <hugo.chaloyard@le-campus-numerique.fr>
2024-06-07 10:21:44 +0200

    fix: pre-commit

--- Hugo/mariadb.sh
+++ Hugo/mariadb.sh
@@ -8,18 +8,9 @@ PROJECT_NAME=$4
 
 ### Install MariaDB ###
 sudo apt-get install -y mariadb-server
-sudo mariadb-secure-installation <<EOF
-n
-n
-n
-Y
-Y
-Y
-Y
-EOF
 
 ### Create Database and User ###
-<<EOF
+sudo mariadb -u root <<EOF
 CREATE DATABASE $PROJECT_NAME;
 CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASS';
 GRANT ALL PRIVILEGES ON $PROJECT_NAME.* TO '$DB_USER'@'localhost';
