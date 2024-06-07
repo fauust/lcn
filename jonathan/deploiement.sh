98b64dbd5da13a275b59e43d0587e9449665aa99
jonathan-luscap <jonathan.luscap@le-campus-numerique.fr>
2024-06-07 11:11:09 +0200

    apache php composer creation du projet laravel

--- jonathan/deploiement.sh
+++ jonathan/deploiement.sh
@@ -13,6 +13,10 @@ ssh -o "StrictHostKeyChecking no" jon@192.168.122.42 'bash /home/jon/install_apa
 
 #installation laravel
 scp -o "StrictHostKeyChecking no" /home/jon/Workspace/autommates/create_vm/lcn_infra_vm/jonathan/install_laravel.sh jon@192.168.122.42:/home/jon/
+scp -o "StrictHostKeyChecking no" /home/jon/Workspace/autommates/create_vm/lcn_infra_vm/jonathan/vm_app.local.conf jon@192.168.122.42:/home/jon/
 ssh -o "StrictHostKeyChecking no" jon@192.168.122.42 'bash /home/jon/install_laravel.sh'
 
-
+#installation mariadb
+#scp -o "StrictHostKeyChecking no" /home/jon/Workspace/autommates/create_vm/lcn_infra_vm/jonathan/install_mariadb.sh jon@192.168.122.42:/home/jon/
+#scp -o "StrictHostKeyChecking no" /home/jon/Workspace/autommates/create_vm/lcn_infra_vm/jonathan/mariadb_user.sql jon@192.168.122.42:/home/jon/
+#ssh -o "StrictHostKeyChecking no" jon@192.168.122.42 'bash /home/jon/install_mariadb.sh'
