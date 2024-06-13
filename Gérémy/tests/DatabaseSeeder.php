a39fd76744abe132c76844717f171216fe371d57
DUMAS <geremydumas@gmail.com>
2024-06-13 14:00:58 +0200

    Added seeder for db

---
+++ Gérémy/tests/DatabaseSeeder.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Article;
+use App\Models\Comment;
+use App\Models\Tag;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $rose = User::factory()->state([
+            'username' => 'Rose',
+            'email' => 'rose@mail.com',
+            'password' => bcrypt('pwd'),
+            'image' => null,
+            'bio' => 'Je voudrais devenir enseignante pour les enfants'
+        ])->make();
+        $rose->save();
+
+        $musonda = User::factory()->state([
+            'username' => 'Musonda',
+            'email' => 'musonda@mail.com',
+            'password' => bcrypt('pwd2'),
+            'image' => null,
+            'bio' => 'Je songe à devenir infirmière, j\'écris mes réflexions'
+        ])->make();
+        $musonda->save();
+
+        $tag = Tag::create([
+            'name' => 'éducation'
+        ]);
+
+        $rose->following()->attach($musonda->id);
+        $musonda->following()->attach($rose->id);
+
+
+        $articleOne = Article::create([
+            'user_id' => $rose->id,
+            'title' => "Mon article",
+            'slug' => "mon-article",
+            'description' => "ceci est une description",
+            'body' => "Mon article est super"
+        ]);
+        $articleTwo = Article::create([
+            'user_id' => $musonda->id,
+            'title' => "Mon article 2",
+            'slug' => "mon-article-2",
+            'description' => "ceci est une description",
+            'body' => "Mon article est super"
+        ]);
+        $articleThree = Article::create([
+            'user_id' => $musonda->id,
+            'title' => "Mon article 3",
+            'slug' => "mon-article-3",
+            'description' => "ceci est une description",
+            'body' => "Mon article est super"
+        ]);
+
+        $musonda->favoritedArticles()->attach($articleOne->id);
+        $rose->favoritedArticles()->attach($articleTwo->id);
+        $rose->favoritedArticles()->attach($articleThree->id);
+
+
+        $articleOne->tags()->attach($tag->id);
+        Comment::create([
+           'article_id' => $articleOne->id,
+           'user_id' => $musonda->id,
+           'body' => 'J\'adore ta manière de concevoir l\'éducation des enfants'
+        ]);
+
+
+    }
+}
