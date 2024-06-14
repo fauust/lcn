96c9ff88793f474e15d5f972201f9be96d11335e
MateoNicoud <mateo.nicoud@le-campus-numerique.fr>
2024-06-14 08:56:59 +0200

    fix

--- mateo/tests/database/seeders/AddRoseArticleSeeder.php
+++ mateo/tests/database/seeders/AddRoseArticleSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use App\Models\Article;
 use App\Models\User;
-use GuzzleHttp\Promise\Create;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
 
@@ -17,23 +16,25 @@ class AddRoseArticleSeeder extends Seeder
      */
     public function run()
     {
+        // Appel du seeder pour créer l'utilisateur Rose si nécessaire
         $this->call(AddRoseUserSeeder::class);
 
+        // Récupération de l'utilisateur Rose
         $rose = User::where('username', 'Rose')->first();
 
-        $exist = Article::where('title', "XVs3zJL")->first();
+        // Vérification si l'article existe déjà
+        $exist = Article::where('slug', 'xvs3zjl')->first();
 
-        $title = "XVs3zJL";
-
-        if(!$exist) {
-            Article::Create([
+        // Si l'article n'existe pas, le créer
+        if (!$exist) {
+            Article::create([
                 'user_id' => $rose->id,
-                'title' => $title,
-                'slug' => Str::slug($title),
-                'description' => Str::random(10),
-                'body' => Str::random(10),
-                'created_at' => now(),
-                'updated_at' => now()
+                'title' => 'XVs3zJL',
+                'slug' => 'xvs3zjl',
+                'description' => 'L5Ro54Luo0',
+                'body' => 'UX8tNbKA8O',
+                'created_at' => '2024-06-13 09:43:02',
+                'updated_at' => '2024-06-13 09:43:02',
             ]);
         }
     }
