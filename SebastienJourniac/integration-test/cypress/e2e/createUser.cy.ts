e5fe5bd0de90edaf807f44f9bfb9b8b44200c183
Sébastien Journiac <152491383+Groughy@users.noreply.github.com>
2024-06-17 15:34:21 +0200

    Third test past !

--- SebastienJourniac/integration-test/cypress/e2e/createUser.cy.ts
+++ SebastienJourniac/integration-test/cypress/e2e/createUser.cy.ts
@@ -18,3 +18,17 @@ describe('list article from this user', () => {
     cy.get('.article-preview').should('contain', 'No articles are here... yet.')
   })
 })
+
+describe('post article', () => {
+  it('jump to post detail page when submit create article form', () => {
+    cy.visit('http://localhost:4137/#/login')
+    cy.get('[placeholder="Email"]').type('foo@example.com')
+    cy.get('[placeholder="Password"]').type('12345678')
+    cy.get('[type="submit"]').click()
+    cy.get('.nav-link').contains('New Post').click()
+    cy.get('[placeholder="Article Title"]').type('Title')
+    cy.get('[placeholder="What\'s this article about?"]').type('content')
+    cy.get('[placeholder="Write your article (in markdown)"]').type('42, j\'en étais sûr !')
+    cy.get('[type="submit"]').click()
+  })
+})
