602bac67124d5bf0e578b5980ba263cc13c3f7bc
Sébastien Journiac <152491383+Groughy@users.noreply.github.com>
2024-06-17 15:26:31 +0200

    Second test past !

--- SebastienJourniac/integration-test/cypress/e2e/createUser.cy.ts
+++ SebastienJourniac/integration-test/cypress/e2e/createUser.cy.ts
@@ -1,17 +1,20 @@
 describe('create user', () => {
-  beforeEach(() => {
-    cy.request({
-      method: 'DELETE',
-      url: 'http://localhost:4137/api/users/foo@example.com', // Remplacez par l'URL de votre API pour supprimer un utilisateur
-      failOnStatusCode: false, // Ne pas échouer si l'utilisateur n'existe pas
-    })
-  })
   it('should create user success when submit a valid form', () => {
     cy.visit('http://localhost:4137/#/register')
     cy.get('[placeholder="Your Name"]').type('foo')
     cy.get('[placeholder="Email"]').type('foo@example.com')
     cy.get('[placeholder="Password"]').type('12345678')
-
     cy.get('[type="submit"]').click()
+  })
+})
+describe('list article from this user', () => {
+  it('should list article from this user', () => {
+    cy.visit('http://localhost:4137/#/login')
+    cy.get('[placeholder="Email"]').type('foo@example.com')
+    cy.get('[placeholder="Password"]').type('12345678')
+    cy.get('[type="submit"]').click()
+    cy.visit('http://localhost:4137/#/')
+    cy.get('.nav-link').contains('foo').click()
+    cy.get('.article-preview').should('contain', 'No articles are here... yet.')
   })
 })
