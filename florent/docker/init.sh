f4078d27cb258003842af489231b8914fdcc268d
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-08-01 13:20:07 +0200

    FEAT: add cmd for install apache & php #2

--- florent/docker/init.sh
+++ florent/docker/init.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 
-#####
 handle_error() {
   read -r -n 1 -s
 }
 
 trap 'handle_error' ERR EXIT
-#####
-
-
 
 # -t: allows you to name your Docker image
 # .: the directory where the Dockerfile is located
@@ -16,10 +12,6 @@ docker build -t sebflo_container .
 
 # -d (detached mode): tells Docker to run the container in the background (detached)
 # -p (port mapping): maps a host machine port to a container port
-docker run --name=sebflo -d -p 666:80 sebflo_container 
+docker run --name=sebflo -d -p 666:80 sebflo_container
 
-
-
-#####
 trap - ERR EXIT
-#####
