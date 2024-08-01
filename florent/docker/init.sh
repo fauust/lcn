0fa5347910d7998e2a3f253ce405786bc48e8abd
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-08-01 11:04:54 +0200

    FEAT: add Dockerfile and script

---
+++ florent/docker/init.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+#####
+handle_error() {
+  read -r -n 1 -s
+}
+
+trap 'handle_error' ERR EXIT
+#####
+
+
+
+# -t: allows you to name your Docker image
+# .: the directory where the Dockerfile is located
+docker build -t sebflo_container .
+
+# -d (detached mode): tells Docker to run the container in the background (detached)
+# -p (port mapping): maps a host machine port to a container port
+docker run -d -p 666:80 sebflo_container
+
+
+
+#####
+trap - ERR EXIT
+#####
