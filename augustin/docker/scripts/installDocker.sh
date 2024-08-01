a3fa6c161f0d4435605cf38beae57689013e6b39
AugustinPech <80385451+AugustinPech@users.noreply.github.com>
2024-08-01 15:39:33 +0200

    modified the scripts switched from apt to apt-get

--- augustin/docker/scripts/installDocker.sh
+++ augustin/docker/scripts/installDocker.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 # make sure your machine is up to date
-sudo apt udate & sudo apt upgrade
+sudo apt-get udate & sudo apt-get upgrade
 # install prerequisites
-sudo apt install apt-transport-https ca-certificates curl software-properties-common gnupg lsb-release -y
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common gnupg lsb-release -y
 # add GPG key
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 # add docker repo to apt sources
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 # make sure your machine is up to date
-sudo apt update & sudo apt upgrade
-sudo apt install docker-ce docker-ce-cli containerd.io -y
+sudo apt-get update & sudo apt-get upgrade
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 sudo apt-get update && sudo apt-get install -y apparmor
 # make sure you are about to install from the Docker repo instead of the default Ubuntu repo:
 test=$(apt-cache policy docker-ce | grep "500")
@@ -20,6 +20,6 @@ if [ "$test" -eq 0 ]; then
 fi
 
 # proper install
-sudo apt install docker.io -y
+sudo apt-get install docker.io -y
 
 sudo usermod -aG docker "$USER"
