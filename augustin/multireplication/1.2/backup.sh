ed28f7696106ce68354e3cb0cda66d42a32373a5
AugustinPech <80385451+AugustinPech@users.noreply.github.com>
2024-08-13 14:07:34 +0200

    fixed some recommandations from the linter

--- augustin/multireplication/1.2/backup.sh
+++ augustin/multireplication/1.2/backup.sh
@@ -6,4 +6,4 @@ BACKUP_FILE="$BACKUP_DIR/mydb_$TIMESTAMP.sql"
 
 mkdir -p $BACKUP_DIR
 
-mariadb-dump -h sql-01 -u root -p"$MYSQL_ROOT_PASSWORD" mydb > $BACKUP_FILE
+mariadb-dump -h sql-01 -u root -p"$MYSQL_ROOT_PASSWORD" mydb > "$BACKUP_FILE"
