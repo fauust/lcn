a8f6090acf3c108d5c532401c8d93afde4b7bc36
Vauquelin Aurelie <aurelie.vauquelin@le-campus-numerique.fr>
2024-08-14 08:15:24 +0200

    docker-compose
    création des deux containers avec mariadb, création d'une table user avec 2 users.

--- Aurelie/multireplication/backup.sh
+++ Aurelie/multireplication/backup.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 # VARIABLES
+#IP_CONTAINER="172.23.0.2"
 IP_CONTAINER="sql-01"
+
+
+# Mettre a jour le docker et installer mysql-client
+docker exec sql-svg sh -c "apt-get update && apt-get install -y mysql-client"
+
+
 
 # Créer le répertoire /backup dans le conteneur sql-svg
 docker exec sql-svg sh -c "mkdir -p /backup"
 
 # Exécuter la commande mariad-dump pour créer la sauvegarde dans le conteneur sql-svg
-docker exec sql-svg sh -c "mariadb-dump -uroot -pa -h ${IP_CONTAINER} mydb > /backup/mydb_$(date +"%Y%m%d%H%M").sql"
+docker exec sql-svg sh -c "mysqldump -uadmin -pa -h ${IP_CONTAINER} mydb > /backup/mydb_$(date +"%Y%m%d%H%M").sql"
