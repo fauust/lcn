546f6d1d6f23db7d8b5d7f014f71165f20ac5fbf
Florent <florent.sartout@le-campus-numerique.fr>
2024-08-29 15:29:54 +0200

    REFACTO

--- florent/monitoring/bash/functions.sh
+++ florent/monitoring/bash/functions.sh
@@ -2,6 +2,13 @@
 
 # shellcheck disable=SC2155
 
+get_pod_name(){
+  local POD_PREFIX=$1
+  local CMD=$(kubectl get pods --no-headers --sort-by=.metadata.creationTimestamp -o custom-columns=":metadata.name" | grep "^$POD_PREFIX-" | tail -n 1)
+  local GET_POD_NAME=$CMD
+  echo "$GET_POD_NAME"
+}
+
 is_deleting_pods_complete() {
   while true; do
     local CMD=$(kubectl get pods --all-namespaces --no-headers -o custom-columns=":metadata.namespace,:metadata.name" | grep -vE "$(IFS=\|; echo "${NAMESPACES_TO_KEEP[*]}")")
@@ -18,55 +25,35 @@ is_deleting_pods_complete() {
   done
 }
 
-get_pod_name(){
-  local POD_PREFIX=$1
-  local CMD=$(kubectl get pods --no-headers --sort-by=.metadata.creationTimestamp -o custom-columns=":metadata.name" | grep "^$POD_PREFIX-" | tail -n 1)
-  local GET_POD_NAME=$CMD
-  echo "$GET_POD_NAME"
-}
-
-is_pod_running() {
+is_sql_table_complete() {
   local POD_NAME=$1
 
   while true; do
-    local CMD=$(kubectl get pod "$POD_NAME" -o jsonpath='{.status.phase}')
-    local POD_STATUS=$CMD
+    RES=$(kubectl exec "$POD_NAME" -- mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -D "$MYSQL_DB" -e "SHOW TABLES LIKE '$MYSQL_TABLE';" 2>/dev/null)
 
-    if [[ "$POD_STATUS" == "Running" ]]; then
-      echo "The pod [ $POD_NAME ] is running!"
+    if [[ "$RES" == *"$MYSQL_TABLE"* ]]; then
+      echo "The [ $MYSQL_TABLE ] table is configured!"
       return 0
     else
-      echo "The pod [ $POD_NAME ] is not yet running"
-      echo "Status: $POD_STATUS..."
+      echo "The [ $MYSQL_TABLE ] table was not found..."
       sleep 5
     fi
   done
 }
 
-is_dashboard_ready() {
+is_database_ready() {
     local POD_PREFIX=$1
     local POD_NAME=$(get_pod_name "$POD_PREFIX")
 
-    is_pod_running "$POD_NAME"
+    is_sql_table_complete "$POD_NAME"
 
-    while true; do
-      local CMD=$(kubectl exec "$POD_NAME" -- wget --server-response --quiet --output-document=/dev/null "$DASHBOARD_URL" 2>&1 | grep "HTTP/" | awk '{print $2}')
-      local DASHBOARD_STATUS=$CMD
-
-      if [[ "$DASHBOARD_STATUS" == "200" ]]; then
-        echo "The Zabbix dashboard is now ready!"
-        return 0
-      else
-        echo "Waiting for the Zabbix dashboard to be ready..."
-        sleep 5
-      fi
-    done
+    ### WARNING
+    # Database error
+    # Unable to select configuration.
 }
 
 init_zabbix_dashboard(){
-  is_dashboard_ready "$SERVER_POD_PREFIX"
-
+  is_database_ready "$MYSQL_POD_PREFIX"
   echo "Zabbix dashboard launched..."
-
-  firefox --new-window "$DASHBOARD_URL" --silent
+  firefox --new-tab "$DASHBOARD_URL" --silent
 }
--- florent/monitoring/bash/vars.sh
+++ florent/monitoring/bash/vars.sh
@@ -6,10 +6,13 @@ export NAMESPACES_TO_KEEP=("default" "kube-node-lease" "kube-public" "kube-syste
 
 export DASHBOARD_URL=http://192.168.49.2:30080
 
+export MYSQL_POD_PREFIX=zabbix-mysql
 export SERVER_POD_PREFIX=zabbix-server
 
 export MYSQL_USER=zabbix
 export MYSQL_PASSWORD=root
+export MYSQL_DB=zabbix
+export MYSQL_TABLE=dbversion
 
 export RETRY_INTERVAL=1
 export MAX_RETRIES=60
